187ee66aa3e8bebf51148ee4dc549bd42bff4f4e
thiagosm17 <thiagoschwartzmachado@gmail.com>
2024-05-18 12:35:37 -0300

    Added class State to handle all the state guesses

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas
+from state import State
 
 
 unitedStatesMap = turtle.Screen()
@@ -10,14 +11,19 @@ turtle.shape("blank_states_img.gif")
 states = pandas.read_csv("50_states.csv")
 game_on = True
 points = 0
+correctGuesses = []
 
 while game_on:
-    answer_input = unitedStatesMap.textinput(title="Guess the state", prompt="Guess the state")
-    for eachState in states.state:
-        if answer_input == eachState:
-            points += 1
-            print(points)
-    if answer_input == "Cancel" or points == 50:
+    user_answer = unitedStatesMap.textinput(title=f"Guess the state {points}/50", prompt="Guess the state")
+    current_guess = State(states, user_answer, correctGuesses)
+    if current_guess.check_if_the_guess_is_correct():
+        current_guess.write_current_guess()
+        correctGuesses.append(user_answer)
+        points += 1
+
+    if user_answer == "Cancel" or points >= 50:
         game_on = False
 
 turtle.mainloop()
+
+
---
+++ state.py
@@ -0,0 +1,29 @@
+import turtle
+
+
+class State(turtle.Turtle):
+    def __init__(self, states, answer_input, correctGuesses):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("Black")
+        self.states = states
+        self.correctGuesses = correctGuesses
+        self.answer_input = answer_input
+
+    def is_it_already_guessed(self):
+        for guesses in self.correctGuesses:
+            if self.answer_input == guesses:
+                return True
+        return False
+
+    def check_if_the_guess_is_correct(self):
+        for eachState in self.states.state:
+            if self.answer_input.lower() == eachState.lower() and not (self.is_it_already_guessed()):
+                return True
+        return False
+
+    def write_current_guess(self):
+        location = self.states[self.states["state"] == self.answer_input]
+        self.goto(x=int(location.x), y=int(location.y))
+        self.write(self.answer_input)
